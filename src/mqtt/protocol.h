787507a304390df119002e5534c8d82a59ba9f7f
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2025-09-21 00:37:56 +0800

    simplify protocol.h

--- src/mqtt/protocol.h
+++ src/mqtt/protocol.h
@@ -14,23 +14,20 @@
   X(RECORDER, "audio_biometric/slainless/device/recorder")                     \
   X(CONTROLLER, "audio_biometric/slainless/device/controller")
 
-namespace MqttHeader {
 #define X(name, value) constexpr char name[] = value;
+namespace MqttHeader {
 MQTT_HEADER
-#undef X
 } // namespace MqttHeader
 
 namespace MqttMessageType {
-#define X(name, value) constexpr char name[] = value;
 MQTT_MESSAGE_TYPE
-#undef X
 } // namespace MqttMessageType
 
 namespace MqttTopic {
-#define X(name, value) constexpr char name[] = value;
 MQTT_TOPIC
-#undef X
 } // namespace MqttTopic
+
+#undef X
 
 extern "C" {
 const char *ffi_mqttProtocol(const char *protocolKey, const char *key);
