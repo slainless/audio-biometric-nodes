16c46b994ea2501597aad070fca4c3d4257dd7f8
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2025-09-20 20:40:33 +0800

    shared protocol definition

---
+++ src/mqtt/protocol.cpp
@@ -0,0 +1,29 @@
+#include "mqtt/protocol.h"
+
+#include <cstring>
+
+extern "C" {
+const char *ffi_mqttProtocol(const char *protocolKey, const char *key) {
+  if (!key)
+    return nullptr;
+
+#define X(name, value)                                                         \
+  if (strcmp(key, #name) == 0)                                                 \
+    return PX::name;
+
+  if (strcmp(protocolKey, "MqttHeader") == 0) {
+#define PX MqttHeader
+    MQTT_HEADER
+#undef PX
+  } else if (strcmp(protocolKey, "MqttMessageType") == 0) {
+#define PX MqttMessageType
+    MQTT_MESSAGE_TYPE
+#undef PX
+  } else if (strcmp(protocolKey, "MqttTopic") == 0) {
+#define PX MqttTopic
+    MQTT_TOPIC
+#undef PX
+  }
+  return nullptr;
+}
+}
--- src/mqtt/protocol.h
+++ src/mqtt/protocol.h
@@ -1,17 +1,37 @@
 #pragma once
+
+#define MQTT_HEADER                                                            \
+  X(WILL, "ded")                                                               \
+  X(VERIFY, "verify")
+
+#define MQTT_MESSAGE_TYPE                                                      \
+  X(MESSAGE, "msg")                                                            \
+  X(FRAGMENT_HEADER, "head")                                                   \
+  X(FRAGMENT_BODY, "frag")                                                     \
+  X(FRAGMENT_TRAILER, "end")
+
+#define MQTT_TOPIC                                                             \
+  X(RECORDER, "audio_biometric/slainless/device/recorder")                     \
+  X(CONTROLLER, "audio_biometric/slainless/device/controller")
+
 namespace MqttHeader {
-constexpr char WILL[] = "ded";
-constexpr char VERIFY[] = "verify";
+#define X(name, value) constexpr char name[] = value;
+MQTT_HEADER
+#undef X
 } // namespace MqttHeader
 
 namespace MqttMessageType {
-constexpr char MESSAGE[] = "msg";
-
-constexpr char FRAGMENT_HEADER[] = "head";
-constexpr char FRAGMENT_BODY[] = "frag";
-constexpr char FRAGMENT_TRAILER[] = "end";
+#define X(name, value) constexpr char name[] = value;
+MQTT_MESSAGE_TYPE
+#undef X
 } // namespace MqttMessageType
 
 namespace MqttTopic {
-constexpr char RECORDER[] = "audio_biometric/slainless/device/recorder";
+#define X(name, value) constexpr char name[] = value;
+MQTT_TOPIC
+#undef X
+} // namespace MqttTopic
+
+extern "C" {
+const char *ffi_mqttProtocol(const char *protocolKey, const char *key);
 }
