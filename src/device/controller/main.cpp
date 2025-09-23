67a575058b2b590033d087612af1e671c6295d5c
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2025-09-23 22:40:17 +0800

    base setup for the controller

---
+++ src/device/controller/main.cpp
@@ -0,0 +1,51 @@
+#include "core/mqtt.h"
+#include "setup/wifi.h"
+
+#include "device/controller/controller.h"
+
+#include <SPIFFS.h>
+
+Mqtt mqtt(CONTROLLER_IDENTIFIER);
+
+void setup()
+{
+  Serial.begin(115200);
+
+  if (!SPIFFS.begin(true))
+  {
+    while (true)
+    {
+      Serial.println("These components are missing:");
+      Serial.println("- SPIFFS");
+      Serial.println();
+      delay(1000);
+    }
+  }
+
+  setupWiFi();
+  setupMqtt(mqtt);
+}
+
+void reconnectHandler() { reconnectMqtt(mqtt); }
+
+void loop()
+{
+  if (!mqtt.client)
+  {
+    Serial.println("MQTT client is not initialized, please setup mqtt");
+  }
+
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("WiFi is not connected, please setup wifi");
+  }
+
+  mqtt.poll(reconnectHandler);
+
+  auto cmd = Serial.readStringUntil('\n');
+  cmd.trim();
+  if (cmd.equalsIgnoreCase("wifi"))
+    return configureWiFi();
+  if (cmd.equalsIgnoreCase("mqtt"))
+    return configureMqtt(mqtt);
+}
