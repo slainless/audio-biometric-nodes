08776d7e058228c1257842743a2f09af70cee641
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2025-09-23 17:00:28 +0800

    add mqtt verification handler

---
+++ src/mqtt/core/verificator.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+from .server import MqttServer
+from .ffi import Protocol
+
+from ...biometric import Verificator
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class VerificationHandler:
+    def __init__(self, verificator: Verificator):
+        self.verificator = verificator
+
+    def __call__(self, server: MqttServer, id: str, data: bytes) -> Any:
+        result = self.verificator.verify(data)
+        _logger.info(f"[{id}] Verification result:\n{result}")
+
+        if not result.verified:
+            _logger.info(f"[{id}] Verification failed")
+            return
+
+        if result.command is None:
+            _logger.info(f"[{id}] No command found")
+            return
+
+        if result.command not in Protocol.MqttControllerCommand.Values:
+            _logger.info(f"[{id}] Unknown command '{result.command}'")
+            return
+
+        _logger.info(f"[{id}] Sending command '{result.command}'")
+        server.send_command("", result.command)
