b264af463bf6f8e89befce7365915f15e2233384
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2025-09-23 16:11:58 +0800

    flatten main.py

--- src/server/main.py
+++ src/server/main.py
@@ -3,9 +3,8 @@ import os
 from pathlib import Path
 
 from fastapi import FastAPI
-import uvicorn
 
-from server.lifecycle import BiometricServerLifecycle
+from .lifecycle import BiometricServerLifecycle
 
 from ..biometric import (
     SpeechbrainEmbedder,
@@ -30,29 +29,27 @@ RECORDER_TOPIC = Protocol.MqttTopic.RECORDER
 
 _logger = logging.getLogger(__name__)
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
-    embedding_source = FileEmbeddingSource(EMBEDDING_FILE_PATH)
-    command_matcher = DiffCommandMatcher(
-        {
-            "nyalakan lampu": "LAMP_ON",
-            "matikan lampu": "LAMP_OFF",
-            "hidupkan lampu": "LAMP_ON",
-            "tutup lampu": "LAMP_OFF",
-            "buka lampu": "LAMP_ON",
-            "lampu hidup": "LAMP_ON",
-            "lampu mati": "LAMP_OFF",
-        }
-    )
-    verificator = Verificator(
-        command_matcher, SpeechbrainEmbedder(embedding_source), VoxtralTranscriber()
-    )
+embedding_source = FileEmbeddingSource(EMBEDDING_FILE_PATH)
+command_matcher = DiffCommandMatcher(
+    {
+        "nyalakan lampu": "LAMP_ON",
+        "matikan lampu": "LAMP_OFF",
+        "hidupkan lampu": "LAMP_ON",
+        "tutup lampu": "LAMP_OFF",
+        "buka lampu": "LAMP_ON",
+        "lampu hidup": "LAMP_ON",
+        "lampu mati": "LAMP_OFF",
+    }
+)
+verificator = Verificator(
+    command_matcher, SpeechbrainEmbedder(embedding_source), VoxtralTranscriber()
+)
 
-    api_server = ApiServer(verificator)
-    mqtt_server = MqttServer(
-        MQTT_BROKER_HOST, MQTT_BROKER_PORT, RECORDER_TOPIC, MQTT_KEEPALIVE
-    )
+api_server = ApiServer(verificator)
+mqtt_server = MqttServer(
+    MQTT_BROKER_HOST, MQTT_BROKER_PORT, RECORDER_TOPIC, MQTT_KEEPALIVE
+)
 
-    app = FastAPI(lifespan=BiometricServerLifecycle(mqtt_server, api_server).lifespan)
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+app = FastAPI(lifespan=BiometricServerLifecycle(mqtt_server, api_server).lifespan)
